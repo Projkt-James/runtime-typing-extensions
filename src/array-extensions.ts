3497abb53f1d095037fa0c37c4ddc55399771f7b
James Hanford <james@projkt.io>
2019-10-02 17:32:55 +1000

    Updated Array extensions to improve runtime typechecking

--- src/array-extensions.ts
+++ src/array-extensions.ts
@@ -1,12 +1,32 @@
 import TypeValidator from "./type-validator";
 
 declare global {
+    export interface ArrayConstructor {
+        isNullOrUndefined<T>(arr: Array<T> | undefined | null): boolean;
+        isNullOrEmpty<T>(arr: Array<T> | undefined | null): boolean;
+    }
     export interface Array<T> {
+        isArray(): boolean;
         isIndexType(type: new (...args: any[]) => Boolean | Number | String | Object, index?: number): boolean;
     }
-
 }
 
+Array.isNullOrUndefined = function <T>(arr: Array<T> | undefined | null): boolean {
+    if (arr === null || TypeValidator.isUndefined(arr)) {
+        return true;
+    } else if (Array.isArray(arr)) return false; 
+    throw new Error("TypeError: Provided param is not of type 'array'");
+}
+Array.isNullOrEmpty = function <T>(arr: Array<T> | undefined | null): boolean {
+    if (arr === null || TypeValidator.isUndefined(arr)) {
+        return true;
+    } else if (Array.isArray(arr)) return (arr!.length <= 0);
+    throw new Error("TypeError: Provided param is not of type 'array'");
+}
+Array.prototype.isArray = function (): boolean {
+    if (Array.isArray) return Array.isArray(Array.from(this));
+    return TypeValidator.isArray(Array.from(this));
+}
 Array.prototype.isIndexType = function (type: new (...args: any[]) => Boolean | Number | String | Object, index?: number): boolean {
     return type.name === (Array.from(this)[TypeValidator.isNumber(index) ? index! : 0]).constructor.name;
 }
--- tests/array-extensions.test.ts
+++ tests/array-extensions.test.ts
@@ -1,6 +1,86 @@
 import "../src/index";
 
+//
+//  isNullOrUndefined
+//
+test('null isNullOrUndefined returns true', async () => {
+    expect(Array.isNullOrUndefined(null)).toBe(true);
+});
+test('undefined isNullOrUndefined returns true', async () => {
+    expect(Array.isNullOrUndefined(undefined)).toBe(true);
+});
+
+test('empty array isNullOrUndefined returns false', async () => {
+    expect(Array.isNullOrUndefined([])).toBe(false);
+});
+test('boolean[] isNullOrUndefined returns false', async () => {
+    expect(Array.isNullOrUndefined([true])).toBe(false);
+});
+test('number[] isNullOrUndefined returns false', async () => {
+    expect(Array.isNullOrUndefined([0])).toBe(false);
+});
+
+test('string isNullOrUndefined throws TypeError', async () => {
+    try {
+        // @ts-ignore
+        Array.isNullOrUndefined("");
+    } catch (e) {
+        expect(e.name).toBe("Error");
+        expect(e.message).toBe("TypeError: Provided param is not of type 'array'");
+    }
+});
+test('number isNullOrUndefined throws TypeError', async () => {
+    try {
+        // @ts-ignore
+        Array.isNullOrUndefined(0);
+    } catch (e) {
+        expect(e.name).toBe("Error");
+        expect(e.message).toBe("TypeError: Provided param is not of type 'array'");
+    }
+});
+
+//
+//  Array.isNullOrEmpty
+//
+test('null isNullOrEmpty returns true', async () => {
+    expect(Array.isNullOrEmpty(null)).toBe(true);
+});
+test('undefined isNullOrEmpty returns true', async () => {
+    expect(Array.isNullOrEmpty(undefined)).toBe(true);
+});
+test('empty array isNullOrEmpty returns true', async () => {
+    expect(Array.isNullOrEmpty([])).toBe(true);
+});
+
+test('boolean[] isNullOrEmpty returns false', async () => {
+    expect(Array.isNullOrEmpty([true])).toBe(false);
+});
+test('number[] isNullOrEmpty returns false', async () => {
+    expect(Array.isNullOrEmpty([0])).toBe(false);
+});
+
+test('string isNullOrEmpty throws TypeError', async () => {
+    try {
+        // @ts-ignore
+        Array.isNullOrEmpty("");
+    } catch (e) {
+        expect(e.name).toBe("Error");
+        expect(e.message).toBe("TypeError: Provided param is not of type 'array'");
+    }
+});
+test('number isNullOrEmpty throws TypeError', async () => {
+    try {
+        // @ts-ignore
+        Array.isNullOrEmpty(0);
+    } catch (e) {
+        expect(e.name).toBe("Error");
+        expect(e.message).toBe("TypeError: Provided param is not of type 'array'");
+    }
+});
+
+//
 // isIndexType - Boolean
+//
 test('boolean[] isIndexType(Boolean) returns true', async () => {
     expect([true, false].isIndexType(Boolean)).toBe(true);
 });
@@ -11,7 +91,9 @@ test('string[] isIndexType(Boolean) returns false', async () => {
     expect(['', ''].isIndexType(Boolean)).toBe(false);
 });
 
+//
 // isIndexType - String
+//
 test('string[] isIndexType(String) returns true', async () => {
     expect(['string1', 'string2'].isIndexType(String)).toBe(true);
 });
@@ -22,7 +104,9 @@ test('boolean[] isIndexType(Number) returns false', async () => {
     expect([true, false].isIndexType(String)).toBe(false);
 });
 
+//
 // isIndexType - Number
+//
 test('number[] isIndexType(Number) returns true', async () => {
     expect([100, 200].isIndexType(Number)).toBe(true);
 });
